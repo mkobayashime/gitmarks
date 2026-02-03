c3e3a5a8b87b151c6d784ff23de6ddf1f6bb1486
Masaki Kobayashi <contact@mkobayashi.me>
2026-02-03 19:04:28 +0900

    Update Extension icon and make use of `@wxt-dev/auto-icons`

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,11 +5,14 @@ validateEnvVars();
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-	modules: ["@wxt-dev/module-react"],
+	modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
 	manifest: {
 		name: "GitMarks",
 		permissions: ["storage"],
 		host_permissions: ["https://github.com/*", "https://api.github.com/*"],
+	},
+	autoIcons: {
+		baseIconPath: "public/icon.svg",
 	},
 	outDirTemplate: "{{browser}}-mv{{manifestVersion}}",
 });

375b2136fe866436c91f8138b4e02c2b17b4116c
Masaki Kobayashi <contact@mkobayashi.me>
2026-02-04 01:31:27 +0900

    Add background service worker with hourly alarm-based sync
    Creates a "gitmarks-sync" alarm on install that fires syncAllConnections every 60 minutes.

---
+++ entrypoints/background.ts
@@ -0,0 +1,16 @@
+import { syncAllConnections } from "../lib/sync/sync-all.ts";
+
+export default defineBackground(() => {
+	const SYNC_ALARM_NAME = "gitmarks-sync";
+	const SYNC_INTERVAL_MINUTES = 60;
+
+	browser.runtime.onInstalled.addListener(() => {
+		browser.alarms.create(SYNC_ALARM_NAME, { periodInMinutes: SYNC_INTERVAL_MINUTES });
+	});
+
+	browser.alarms.onAlarm.addListener((alarm) => {
+		if (alarm.name === SYNC_ALARM_NAME) {
+			void syncAllConnections();
+		}
+	});
+});
